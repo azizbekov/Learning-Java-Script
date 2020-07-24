09c336d671bbc420bfc4a1000227e98bf5fe7095
azizbek <azizbekozodov@gmail.com>
2020-07-24 23:24:13 +0500

    Javascript

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// let number = 5;
+
+
+// number = 10;
+// console.log(number);
+
+
+
+
+
+// const obj = {
+// 	oila: "uylangan",
+// 	ismi: "palonchi",
+// 	tugilgan: 1987
+
+// };
+ 
+
+// console.log(obj.oila);
+
+// const answer = prompt("Вам есть 18?", "90");
+// console.log(answer);
+
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколька вам лет?', '');
+
