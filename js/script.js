ccb34907b8bf39cc59f4432b81bafef1154ce735
azizbek <azizbekozodov@gmail.com>
2020-07-25 16:46:24 +0500

    sikli

--- js/script.js
+++ js/script.js
@@ -5,15 +5,24 @@ const personalMovieDB = {
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: true
+	privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	  b = prompt('На сколько оцените его?', ''),
-	  c = prompt('Один из последних просмотренных фильмов?', ''),
-	  d = prompt('На сколько оцените его?', '');
-	 
-	personalMovieDB.movies[a] = b;
-	personalMovieDB.movies[c] = d;
+
+
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из последних просмотренных фильмов?', ''),
+				b = prompt('На сколько оцените его?', '');
+		
+		if (a != null && b != null && a != '' && b != '' && a.lenght < 6) {
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+		} else {
+			console.log('error');
+			i--; 
+		}
+	
+	}
+
 
 	console.log(personalMovieDB);
