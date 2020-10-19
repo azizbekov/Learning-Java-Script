1c958e475e8cd4f9b158c62be403d1eedc0bb139
Aziz <azizbekozodov@gmail.com>
2020-10-19 18:20:08 +0500

    yengi

--- js/script.js
+++ js/script.js
@@ -7,18 +7,34 @@ const personalMovieDB = {
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
+	privat: true
 };
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-	  b = prompt('На сколько оцените его?', ''),
-	  c = prompt('Один из просмотренных фильмов?', ''),
-	  d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из просмотренных фильмов?', ''),
+		  b = prompt('На сколько оцените его?', '');
+		  
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+	
+}
+
+if (personalMovieDB.count < 10) {
+	console.log("Посмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
 
 
- 
